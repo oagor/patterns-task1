65c14cc685c2c7ef48e8895bb2eb500b042aa432
Olga Gorban <oatyanina17@gmail.com>
2026-08-15 11:37:28 +0300

    Fix notification text check

--- src/test/java/ru/netology/delivery/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/DeliveryTest.java
@@ -1,5 +1,6 @@
 package ru.netology.delivery;
 
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -8,7 +9,6 @@ import ru.netology.delivery.data.DataGenerator;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -22,13 +22,11 @@ class DeliveryTest {
     @Test
     @DisplayName("Should successful plan and replan meeting")
     void shouldSuccessfulPlanAndReplanMeeting() {
+
         var validUser = DataGenerator.Registration.generateUser("ru");
 
         var daysToAddForFirstMeeting = 4;
-        var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
-
         var daysToAddForSecondMeeting = 7;
-        var secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
 
         var firstMeetingDateForInput = LocalDate.now()
                 .plusDays(daysToAddForFirstMeeting)
@@ -38,42 +36,57 @@ class DeliveryTest {
                 .plusDays(daysToAddForSecondMeeting)
                 .format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
 
+        // Заполняем город
         $("[data-test-id='city'] input")
                 .setValue(validUser.getCity());
 
+        // Заполняем первую дату
         $("[data-test-id='date'] input")
                 .setValue(firstMeetingDateForInput);
 
+        // Заполняем имя
         $("[data-test-id='name'] input")
                 .setValue(validUser.getName());
 
+        // Заполняем телефон
         $("[data-test-id='phone'] input")
                 .setValue(validUser.getPhone());
 
+        // Соглашаемся с условиями
         $("[data-test-id='agreement']")
                 .click();
 
+        // Первое планирование
         $$("button")
-                .findBy(text("Запланировать"))
+                .findBy(Condition.text("Запланировать"))
                 .click();
 
+        // Проверяем успешное планирование
         $("[data-test-id='success-notification']")
                 .shouldBe(visible);
+
+        // Меняем дату на вторую
+        $("[data-test-id='date'] input")
+                .clear();
 
         $("[data-test-id='date'] input")
                 .setValue(secondMeetingDateForInput);
 
+        // Нажимаем "Запланировать"
         $$("button")
-                .findBy(text("Запланировать"))
+                .findBy(Condition.text("Запланировать"))
                 .click();
 
+        // Проверяем сообщение о необходимости перепланирования
         $("[data-test-id='replan-notification']")
                 .shouldBe(visible);
 
+        // Подтверждаем перепланирование
         $$("button")
-                .findBy(text("Перепланировать"))
+                .findBy(Condition.text("Перепланировать"))
                 .click();
 
+        // Проверяем успешное перепланирование
         $("[data-test-id='success-notification']")
                 .shouldBe(visible);
     }
