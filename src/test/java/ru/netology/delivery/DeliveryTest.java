ab46912353cf08fa27f89bb9694ca598981fdae0
Olga Gorban <oatyanina17@gmail.com>
2026-08-15 11:41:39 +0300

    Update delivery test

--- src/test/java/ru/netology/delivery/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/DeliveryTest.java
@@ -36,57 +36,45 @@ class DeliveryTest {
                 .plusDays(daysToAddForSecondMeeting)
                 .format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
 
-        // Заполняем город
         $("[data-test-id='city'] input")
                 .setValue(validUser.getCity());
 
-        // Заполняем первую дату
         $("[data-test-id='date'] input")
                 .setValue(firstMeetingDateForInput);
 
-        // Заполняем имя
         $("[data-test-id='name'] input")
                 .setValue(validUser.getName());
 
-        // Заполняем телефон
         $("[data-test-id='phone'] input")
                 .setValue(validUser.getPhone());
 
-        // Соглашаемся с условиями
         $("[data-test-id='agreement']")
                 .click();
 
-        // Первое планирование
         $$("button")
                 .findBy(Condition.text("Запланировать"))
                 .click();
 
-        // Проверяем успешное планирование
         $("[data-test-id='success-notification']")
                 .shouldBe(visible);
 
-        // Меняем дату на вторую
         $("[data-test-id='date'] input")
                 .clear();
 
         $("[data-test-id='date'] input")
                 .setValue(secondMeetingDateForInput);
 
-        // Нажимаем "Запланировать"
         $$("button")
                 .findBy(Condition.text("Запланировать"))
                 .click();
 
-        // Проверяем сообщение о необходимости перепланирования
         $("[data-test-id='replan-notification']")
                 .shouldBe(visible);
 
-        // Подтверждаем перепланирование
         $$("button")
                 .findBy(Condition.text("Перепланировать"))
                 .click();
 
-        // Проверяем успешное перепланирование
         $("[data-test-id='success-notification']")
                 .shouldBe(visible);
     }
